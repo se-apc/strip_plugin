c1556791cc66ea8fdf3653c7999a2421565f170b
Michael Schmidt <Michael.K.Schmidt@schneider-electric.com>
2019-01-28 19:06:34 +0000

    First pass at package for hex.pm

--- mix.exs
+++ mix.exs
@@ -6,8 +6,11 @@ defmodule StripPlugin.MixProject do
       app: :strip_plugin,
       version: "0.1.0",
       elixir: "~> 1.6",
+      description: description(),
+      package: package(),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      source_url: "https://github.com/se-apc/strip_plugin"
     ]
   end
 
@@ -21,5 +24,18 @@ defmodule StripPlugin.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [{:distillery, "~> 2.0"}]
+  end
+
+  defp description() do
+    "A Distillery Plugin to safely strip Elixir Release Builds"
+  end
+
+  defp package() do
+    [
+      # These are the default files included in the package
+      files: ~w(lib .formatter.exs mix.exs README* LICENSE*),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/se-apc/strip_plugin"}
+    ]
   end
 end
